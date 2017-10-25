959df4f4cb4f1c40febb8220d9d6a9b496269f14
Unknown <roger.w.campbell@ttu.edu>
2017-10-25 18:47:04 -0500

    Added dictionaries with unit cost
    Unit costs per race, keyed by unit name with values of mineral cost + vespene cost.

---
+++ agents/costs.py
@@ -0,0 +1,14 @@
+TerranCosts={}
+TerranCosts= {'SCV': 50+0,'Marine':50+0,'MULE': 50,'Marauder':100+25,'Reaper':50+50,'Ghost':200+100,'Hellion':100+0,\
+              'Hellbat':100+0,'Widow Mine':75+25,'Siege Tank':150+125,'Cyclone'150+100,'Thor':300+200,'Viking':150+75,\
+              'Viking':150+75,'Medivac':100+100,'Liberator':150+150,'Banshee':150+100,'Raven':100+200,'Battlecruiser':400+300,\
+              'Planetary Fortress':150+150,'Missile Turret':100+0,'Auto-Turret':50,'Point Defense Drone':50}
+ProtossCosts={}
+ProtossCosts={'Probe':50+0,'Zealot':100+0,'Sentry':50+100,'Stalker':125+50,'Adept':100+25,'High Templar':50+150,'Dark Templar':125+125,\
+              'Archon':250+250,'Observer':25+75,'Warp Prism':200+0,'Immortal':250+100,'Colossus':300+200,'Disruptor':150+150,'Phoenix':150+100,\
+              'Void Ray':250+150,'Oracle':150+150,'Tempest':300+200,'Carrier':350+250,'Interceptor':5+0,'Mothership Core':100+100,'Mothership':300+300}
+ZergCosts={}
+ZergCosts={'Drone':50+0,'Queen':150+0,'Zergling':25+0,'Baneling':25+25,'Roach':75+25,'Ravager':25+75,'Hydralisk':100+50,'Lurker':50+100,\
+           'Infestor':100+150,'Infested Terran':50,'Swarm Host':100+75,'Locust':50,'Ultralisk':300+200,'Overlord':100+0,'Overseer':50+50,\
+           'Changeling':50,'Mutalisk':100+100,'Corruptor':150+100,'Viper':100+200,'Brood Lord':150+150,'Broodling':50,'Spine Crawler':100,\
+           'Spore Crawler':75,'Nydus Worm':100+100}
