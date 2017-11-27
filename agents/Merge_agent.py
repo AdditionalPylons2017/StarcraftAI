5ff6abf26fc265b3d8c9ecfb582439150e224cee
Unknown <roger.w.campbell@ttu.edu>
2017-11-27 12:50:07 -0600

    added videos and merge_agent
    videos for demo and merge_agent where successful heuristics are stored

---
+++ agents/Merge_agent.py
@@ -0,0 +1,210 @@
+## Beau Miller
+## Follows tutorial from https://chatbotslife.com/building-a-smart-pysc2-agent-cdc269cb095d
+## originally written by Steven Brown
+
+import random
+import math
+import time
+
+import numpy as np
+import pandas as pd
+
+from pysc2.agents import base_agent
+from pysc2.lib import actions
+from pysc2.lib import features
+
+_NO_OP = actions.FUNCTIONS.no_op.id
+_SELECT_POINT = actions.FUNCTIONS.select_point.id
+_BUILD_SUPPLY_DEPOT = actions.FUNCTIONS.Build_SupplyDepot_screen.id
+_BUILD_BARRACKS = actions.FUNCTIONS.Build_Barracks_screen.id
+_TRAIN_MARINE = actions.FUNCTIONS.Train_Marine_quick.id
+_SELECT_ARMY = actions.FUNCTIONS.select_army.id
+_ATTACK_MINIMAP = actions.FUNCTIONS.Attack_minimap.id
+
+_PLAYER_RELATIVE = features.SCREEN_FEATURES.player_relative.index
+_UNIT_TYPE = features.SCREEN_FEATURES.unit_type.index
+_PLAYER_ID = features.SCREEN_FEATURES.player_id.index
+
+_PLAYER_HOSTILE = 4
+
+_PLAYER_SELF = 1
+
+_TERRAN_COMMANDCENTER = 18
+_TERRAN_SCV = 45
+_TERRAN_SUPPLY_DEPOT = 19
+_TERRAN_BARRACKS = 21
+
+_SCREEN = [0]
+_PLAYER_SELF = 1
+_SUPPLY_USED = 3
+_SUPPLY_MAX = 4
+_NOT_QUEUED = [0]
+_QUEUED = [1]
+
+ACTION_DO_NOTHING = 'donothing'
+ACTION_SELECT_ARMY = 'selectarmy'
+ACTION_ATTACK = 'attack'
+ACTION_ATTACK_RANDOM_ENEMY = 'attack_randomenemy'
+ACTION_ATTACK_TOP_LEFT = 'attack_topleft'
+ACTION_ATTACK_TOP_RIGHT = 'attack_topright'
+ACTION_ATTACK_BOTTOM_LEFT = 'attack_bottomleft'
+ACTION_ATTACK_BOTTOM_RIGHT = 'attack_bottomright'
+
+smart_actions = [
+    ACTION_DO_NOTHING,
+    ACTION_SELECT_ARMY,
+    ACTION_ATTACK_RANDOM_ENEMY
+]
+
+KILL_UNIT_REWARD = 100
+KILL_BUILDING_REWARD = 0.5
+
+
+# Stolen from https://github.com/MorvanZhou/Reinforcement-learning-with-tensorflow
+class QLearningTable:
+    def __init__(self, actions, learning_rate=0.01, reward_decay=0.9, e_greedy=0.9):
+        self.actions = actions  # a list
+        self.lr = learning_rate
+        self.gamma = reward_decay
+        self.epsilon = e_greedy
+        self.q_table = pd.DataFrame(columns=self.actions)
+
+    def choose_action(self, observation):
+        self.check_state_exist(observation)
+
+        if np.random.uniform() < self.epsilon:
+            # choose best action
+            state_action = self.q_table.ix[observation, :]
+
+            # some actions have the same value
+            state_action = state_action.reindex(np.random.permutation(state_action.index))
+
+            action = state_action.idxmax(axis=1)
+        else:
+            # choose random action
+            action = np.random.choice(self.actions)
+
+        return action
+
+    def learn(self, s, a, r, s_):
+        self.check_state_exist(s_)
+        self.check_state_exist(s)
+
+        q_predict = self.q_table.ix[s, a]
+        q_target = r + self.gamma * self.q_table.ix[s_, :].max()
+
+        # update
+        self.q_table.ix[s, a] += self.lr * (q_target - q_predict)
+
+    def check_state_exist(self, state):
+        if state not in self.q_table.index:
+            # append new state to q table
+            self.q_table = self.q_table.append(
+                pd.Series([0] * len(self.actions), index=self.q_table.columns, name=state))
+
+
+class SmartAgent(base_agent.BaseAgent):
+    def __init__(self):
+        super(SmartAgent, self).__init__()
+        self.qlearn = QLearningTable(actions=list(range(len(smart_actions))))
+
+        self.previous_killed_unit_score = 0
+        self.previous_killed_building_score = 0
+
+        self.previous_action = None
+        self.previous_state = None
+
+        # From Base Agent
+        self.reward = 0
+        self.episodes = 0
+        self.steps = 0
+        self.obs_spec = None
+        self.action_spec = None
+
+    def transformDistance(self, x, x_distance, y, y_distance):
+        if not self.base_top_left:
+            return [x - x_distance, y - y_distance]
+
+        return [x + x_distance, y + y_distance]
+
+    def step(self, obs):
+        super(SmartAgent, self).step(obs)
+        player_y, player_x = (obs.observation['minimap'][_PLAYER_RELATIVE] == _PLAYER_SELF).nonzero()
+        self.player_left = 1 if player_x.any() and player_x.mean() <= 31 else 0
+
+        army_supply = obs.observation['player'][5]
+
+        killed_unit_score = obs.observation['score_cumulative'][5]
+        killed_building_score = obs.observation['score_cumulative'][6]
+
+        hot_squares = [0,0,0,0]
+        enemy_y, enemy_x = (obs.observation['minimap'][_PLAYER_RELATIVE] == _PLAYER_HOSTILE).nonzero()
+        enemy_positions = [(enemy_x[i], enemy_y[i]) for i in range(len(enemy_y))]
+        for i in range(0, len(enemy_y)):
+
+            y = int(math.ceil((enemy_y[i]) // 32))
+            x = int(math.ceil((enemy_x[i]) // 32))
+
+            hot_squares[x + (y * 2)] = 1  # indicates which quarter of the screen an enemy is in,
+                                          # [top left, top right, bottom left, bottom right]
+        enemy_count = len(enemy_y)
+
+        current_state = [
+            army_supply,
+            enemy_count,
+            hot_squares[0],
+            hot_squares[1],
+            hot_squares[2],
+            hot_squares[3]
+
+        ]
+
+        if self.previous_action is not None:
+            reward = 0
+
+            if killed_unit_score > self.previous_killed_unit_score:
+                reward += KILL_UNIT_REWARD
+
+
+            self.qlearn.learn(str(self.previous_state), self.previous_action, reward, str(current_state))
+
+        rl_action = self.qlearn.choose_action(str(current_state))
+        smart_action = smart_actions[rl_action]
+
+        self.previous_killed_unit_score = killed_unit_score
+        self.previous_killed_building_score = killed_building_score
+        self.previous_state = current_state
+        self.previous_action = rl_action
+
+        if '_' in smart_action:
+            smart_action, arg = smart_action.split('_')
+
+        if smart_action == ACTION_DO_NOTHING:
+            return actions.FunctionCall(_NO_OP, [])
+
+
+        elif smart_action == ACTION_SELECT_ARMY:
+            if _SELECT_ARMY in obs.observation['available_actions']:
+                return actions.FunctionCall(_SELECT_ARMY, [[0]])
+
+        elif smart_action == ACTION_ATTACK:
+            if _ATTACK_MINIMAP in obs.observation["available_actions"]:
+
+                if arg == 'topleft':
+                    attack_pos = (16,16)
+                elif arg == 'topright':
+                    attack_pos = (48,16)
+                elif arg == 'bottomleft':
+                    attack_pos = (16,48)
+                elif arg == 'bottomright':
+                    attack_pos = (48,48)
+                elif arg == 'randomenemy':
+                    #attack_pos = enemy_positions[np.random.randint(0,len(enemy_positions))]
+                    if len(enemy_positions) > 0:
+                        attack_pos = enemy_positions[0]
+                else:
+                    attack_pos = (0,0)
+
+                return actions.FunctionCall(_ATTACK_MINIMAP, [_NOT_QUEUED, (attack_pos[0], attack_pos[1])])
+
+        return actions.FunctionCall(_NO_OP, [])
